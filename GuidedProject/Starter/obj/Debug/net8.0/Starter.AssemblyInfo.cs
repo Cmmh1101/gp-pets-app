b882cbddc2650d3baf71439177c7d50ac52cfb48
Carla Montano <cm.emprendiendo@gmail.com>
2024-07-10 23:55:02 -0500

    test loop for new pet data

--- GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5222829df5ca89afca26625570df66eddb6009")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fea1593ea70224781f85dd8b0187acee445d63f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
